e839215e5708a740841375128a9c6417f12c26ea
unholyexcalibur911-cmyk <unholyexcalibur911@gmail.com>
2025-10-08 01:20:04 +0800

    .

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,19 @@ import type { NextConfig } from "next";
 import { hostname } from "os";
 
 const nextConfig: NextConfig = {
+  eslint: {
+          ignoreDuringBuilds: true,
+  },
   images: {
     remotePatterns: [
       { 
         protocol: "https",
-        hostname: "committed-comfort-10a5c2f98d.media.strapiapp.com"
+        hostname: "committed-comfort-10a5c2f98d.media.strapiapp.com",    
       },
+      
     ],
   },
 };
+
 
 export default nextConfig;
